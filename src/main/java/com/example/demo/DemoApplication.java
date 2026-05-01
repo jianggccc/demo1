68e8f3e06b1d35d8f70757cbd7004001a916f2d6
jianggc <jianggc.qq.com>
2026-05-01 13:14:43 +0800

    hot-fix commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("hhhhsss");
         System.out.println("dsaccccddddddddxxxxxxxxxxxxxsssssdd");
+        System.out.println("hot-fix commit");
         SpringApplication.run(DemoApplication.class, args);
     }
 
