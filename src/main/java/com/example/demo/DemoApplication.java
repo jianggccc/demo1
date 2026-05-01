c7aabc59764af27d4b0f13f22a1d5ea9633a7c2c
jianggc <jianggc.qq.com>
2026-05-01 13:05:46 +0800

    fff

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("hhhhsss");
-        System.out.println("dsaccccdddddddddd");
+        System.out.println("dsaccccddddddddxxxxxxxxxxxxxsssssdd");
         SpringApplication.run(DemoApplication.class, args);
     }
 
